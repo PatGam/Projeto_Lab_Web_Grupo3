b663b537af5fd3d72805e968801c5c6cf55d638c
LendeaViva <73750123+LendeaViva@users.noreply.github.com>
2021-02-11 17:30:36 +0000

    Displays e mensagens de erro nos Funcionários

--- Models/Funcionarios.cs
+++ Models/Funcionarios.cs
@@ -18,24 +18,38 @@ namespace Projeto_Lab_Web_Grupo3.Models
         [Key]
         [Column("Funcionario_Id")]
         public int FuncionarioId { get; set; }
-        [Required]
-        [StringLength(100)]
+
+        [Required(ErrorMessage = "Preencha o nome do funcionário")]
+        [StringLength(100, ErrorMessage = "O nome não pode ter mais de 100 caracteres")]
+        [Display(Name = "Nome do funcionário")]
         public string Nome { get; set; }
+
+
         [Column("Data_Nascimento", TypeName = "date")]
+        [Display(Name = "Data de nascimento")]
         public DateTime DataNascimento { get; set; }
-        [Required]
-        [StringLength(500)]
+
+        [Required(ErrorMessage = "Preencha a morada do funcionário")]
+        [StringLength(500, ErrorMessage = "A morada não pode ter mais de 500 caracteres")]
+        [Display(Name = "Morada")]
         public string Morada { get; set; }
+
+        [Display(Name = "Contacto de telemóvel")]
         public int Telemovel { get; set; }
-        [Required]
-        [StringLength(100)]
+
+        [Required(ErrorMessage = "Preencha o email do funcionário")]
+        [StringLength(100, ErrorMessage = "O email não pode ter mais de 100 caracteres")]
+        [Display(Name = "E-mail")]
         public string Email { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Preencha o código postal do funcionário")]
         [Column("Codigo_Postal")]
         [StringLength(8)]
+        [Display(Name = "Código Postal")]
         public string CodigoPostal { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Especifique o cargo do funcionário")]
         [StringLength(20)]
+        [Display(Name = "Cargo")]
         public string Role { get; set; }
 
         [InverseProperty("Funcionario")]
