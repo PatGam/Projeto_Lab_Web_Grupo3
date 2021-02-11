be733ccbe5710175b26c870dcb619aa7c0189899
pedrogaspar88@gmail.com <pedrogaspar88@gmail.com>
2021-02-11 16:24:26 +0000

    Função Inserir Promoçoes

---
+++ Data/SeedData.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Projeto_Lab_Web_Grupo3.Models;
+
+
+namespace Projeto_Lab_Web_Grupo3.Data
+{
+    public class SeedData
+    {
+
+        internal static void PreencheDados(Projeto_Lab_WebContext bd)
+        {
+            InserePromocoes(bd);
+            //Insere(bd);
+            //Insere(bd);
+            //Insere(bd);
+            //Insere(bd);
+
+        }
+        //-------------------PROMOÇÕES--------------------------
+        private static void InserePromocoes(Projeto_Lab_WebContext bd)
+        {
+            if (bd.Promocoes.Any()) return;
+
+
+            bd.Promocoes.AddRange(new Promocoes[] {
+                new Promocoes
+                {
+                    //Promocoes_Id="",
+                    Nome="PascoaS",
+                    Descricao="Desconto aplicável durante a época da Páscoa para novas adesões, para pacotes pequenos",
+                    DataInicio=new DateTime(2021,03,01),
+                    DataFim=new DateTime(2021,04,30),
+                    PromocaoDesc=2.99m,
+                },
+                new Promocoes
+                {
+                    //Promocoes_Id="",
+                    Nome="PascoaM",
+                    Descricao="Desconto aplicável durante a época da Páscoa para novas adesões, para pacotes médios",
+                    DataInicio=new DateTime(2021,03,01),
+                    DataFim=new DateTime(2021,04,30),
+                    PromocaoDesc=3.99m,
+                },
+                new Promocoes
+                {
+                    //Promocoes_Id="",
+                    Nome="PascoaL",
+                    Descricao="Desconto aplicável durante a época da Páscoa para novas adesões, para pacotes grandes",
+                    DataInicio=new DateTime(2021,03,01),
+                    DataFim=new DateTime(2021,04,30),
+                    PromocaoDesc=4.99m,
+                },
+                new Promocoes
+                {
+                    //Promocoes_Id="",
+                    Nome="VerãoS",
+                    Descricao="Desconto aplicável durante a época de Verão para novas adesões, para pacotes pequenos",
+                    DataInicio=new DateTime(2021,07,01),
+                    DataFim=new DateTime(2021-08-31),
+                    PromocaoDesc=1.99m,
+                },
+                new Promocoes
+                {
+                    //Promocoes_Id="",
+                    Nome="VerãoM",
+                    Descricao="Desconto aplicável durante a época de Verão para novas adesões, para pacotes médios",
+                    DataInicio=new DateTime(2021,07,01),
+                    DataFim=new DateTime(2021-08-31),
+                    PromocaoDesc=2.59m,
+                },
+                new Promocoes
+                {
+                    //Promocoes_Id="",
+                    Nome="VerãoL",
+                    Descricao="Desconto aplicável durante a época de Verão para novas adesões, para pacotes grandes",
+                    DataInicio=new DateTime(2021,07,01),
+                    DataFim=new DateTime(2021-08-31),
+                    PromocaoDesc=3.99m,
+                },
+                new Promocoes
+                {
+                    //Promocoes_Id="",
+                    Nome="NatalS",
+                    Descricao="Desconto aplicável durante a época de Natal para novas adesões, para pacotes pequenos",
+                    DataInicio=new DateTime(2021,12,01),
+                    DataFim=new DateTime(2022,01,31),
+                    PromocaoDesc=3.99m,
+                },
+                 new Promocoes
+                {
+                    //Promocoes_Id="",
+                    Nome="NatalM",
+                    Descricao="Desconto aplicável durante a época de Natal para novas adesões, para pacotes médios",
+                    DataInicio=new DateTime(2021,12,01),
+                    DataFim=new DateTime(2022,01,31),
+                    PromocaoDesc=4.99m,
+                },
+                  new Promocoes
+                {
+                    //Promocoes_Id="",
+                    Nome="NatalL",
+                    Descricao="Desconto aplicável durante a época de Natal para novas adesões, para pacotes grandes",
+                    DataInicio=new DateTime(2021,12,01),
+                    DataFim=new DateTime(2022,01,31),
+                    PromocaoDesc=5.99m,
+                },
+
+            });
+            bd.SaveChanges();
+        }
+    }
+}
