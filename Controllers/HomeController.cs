75c155f95384396a59fb84ad71ee117097be3593
LendeaViva <73750123+LendeaViva@users.noreply.github.com>
2021-02-12 19:43:03 +0000

    Layout Shared do Admin implementado

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -40,6 +40,11 @@ namespace Projeto_Lab_Web_Grupo3.Controllers
             return View();
         }
 
+        public IActionResult Admin()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
