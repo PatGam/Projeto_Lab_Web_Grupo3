89717a84f88566e4af1481da33d2f8bf09fe19ab
Vitaliy Hurskyy <vitaliyhurskyy@outlook.com>
2021-02-12 09:43:28 +0000

    Atualização do Modelo de Clientes; Atualização do Modelo de Funcionarios;

--- Models/Clientes.cs
+++ Models/Clientes.cs
@@ -24,17 +24,21 @@ namespace Projeto_Lab_Web_Grupo3.Models
         [Column("Data_Nascimento", TypeName = "date")]
         public DateTime DataNascimento { get; set; }
         [Column("NIF")]
+        [StringLength(9, MinimumLength = 9)]
         public int Nif { get; set; }
         [Required]
         [StringLength(200)]
         public string Morada { get; set; }
+        [RegularExpression(@"(9[1236]|2\d)\d{7}", ErrorMessage = "Telefone Inválido")]
+        [StringLength(9, MinimumLength = 9)]
         public int Telemovel { get; set; }
         [Required]
         [StringLength(100)]
         public string Email { get; set; }
         [Required]
         [Column("Codigo_Postal")]
-        [StringLength(8)]
+        [RegularExpression(@"(\d{8}(-\d{4})?", ErrorMessage = "Código Postal Inválido")]
+        [StringLength(8, MinimumLength = 8)]
         public string CodigoPostal { get; set; }
 
         [InverseProperty("Cliente")]
--- Models/Funcionarios.cs
+++ Models/Funcionarios.cs
@@ -44,7 +44,8 @@ namespace Projeto_Lab_Web_Grupo3.Models
 
         [Required(ErrorMessage = "Preencha o código postal do funcionário")]
         [Column("Codigo_Postal")]
-        [StringLength(8)]
+        [RegularExpression(@"(\d{8}(-\d{4})?", ErrorMessage = "Código Postal Inválido")]
+        [StringLength(8, MinimumLength = 8)]
         [Display(Name = "Código Postal")]
         public string CodigoPostal { get; set; }
         [Required(ErrorMessage = "Especifique o cargo do funcionário")]
