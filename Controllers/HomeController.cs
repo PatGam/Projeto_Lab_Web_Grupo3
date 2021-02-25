dd5d05f85b30beb10ef9bf866757f5865aa3b76b
MajestosoPum <dark.mew69@gmail.com>
2021-02-25 03:26:05 +0000

    Adicionada vista Contactos Correção Imagens

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -50,6 +50,11 @@ namespace Projeto_Lab_Web_Grupo3.Controllers
             return View();
         }
 
+        public IActionResult VistaContact()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
