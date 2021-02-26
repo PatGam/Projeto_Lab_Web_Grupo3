e938f06f3b2fd91d247df819516a7d9c5c6a9781
MajestosoPum <dark.mew69@gmail.com>
2021-02-26 14:33:17 +0000

    Correção Secção Imagem inicial correção navbar alteração esquema base de cores

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -55,6 +55,12 @@ namespace Projeto_Lab_Web_Grupo3.Controllers
             return View();
         }
 
+        public IActionResult TVBase()
+        {
+            return View();
+        }
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
