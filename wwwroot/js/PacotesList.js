f455ca4ffe95b4567a1f2ffa8d89c3de740b601e
Vitaliy Hurskyy <vitaliyhurskyy@outlook.com>
2021-03-01 21:19:55 +0000

    Implementação de novo estilo na tabela de Pacotes;

---
+++ wwwroot/js/PacotesList.js
@@ -0,0 +1,67 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#DT_load_Pacote').DataTable({
+        "ajax": {
+            "url": "/pacotes/getall/",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "nome", "width": "30%" },
+            { "data": "preco", "width": "30%" },
+            {
+                "data": "pacoteId",
+                "render": function (data) {
+                    return `<div class="text-center">
+                        <a href="/Pacotes/Edit?id=${data}" class='btn btn btn-warning'>
+                            <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+                        </a>
+                        &nbsp;
+                        <a href="/Pacotes/Details?id=${data}" class='btn btn-primary'>
+                            <i class="fa fa-info-circle" aria-hidden="true"></i>
+                        </a>
+                        &nbsp;
+                        <a class='btn btn-danger ' onclick=Delete('/pacotes/Delete?id='+${data})>
+                            <i class="icon_close_alt2"></i>
+                        </a>
+                        </div>`;
+                }, "width": "40%"
+            }
+        ],
+        "language": {
+            "emptyTable": "no data found"
+        },
+        "width": "100%"
+    });
+}
+
+function Delete(url) {
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
