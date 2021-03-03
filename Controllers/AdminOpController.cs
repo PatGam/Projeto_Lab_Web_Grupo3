97ed3e0580995e7beae6e718d69ffa727ed07c27
LendeaViva <73750123+LendeaViva@users.noreply.github.com>
2021-03-03 12:00:03 +0000

    Vista Home dos Funcionários em progresso

---
+++ Controllers/AdminOpController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Projeto_Lab_Web_Grupo3.Data;
+using Projeto_Lab_Web_Grupo3.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Projeto_Lab_Web_Grupo3.Controllers
+{
+    public class AdminOpController : Controller
+    {
+     
+            private readonly Projeto_Lab_WebContext bd;
+
+            public AdminOpController(Projeto_Lab_WebContext context)
+            {
+                bd = context;
+            }
+
+         
+            public async Task<IActionResult> Index()
+            {
+            List<Pacotes> pacotes = await bd.Pacotes
+              .ToListAsync();
+
+            List<ServicosPacotes> servicospacotes = await bd.ServicosPacotes
+            .ToListAsync();
+
+            List<Servicos> servicos = await bd.Servicos
+            .ToListAsync();
+
+            List<PromocoesPacotes> promocoes = await bd.PromocoesPacotes
+            .ToListAsync();
+
+            HomeGestaoViewModel modelo = new HomeGestaoViewModel
+            {
+                Pacotes = pacotes,
+                ServicosPacotes = servicospacotes,
+                PromocoesPacotes = promocoes,
+                Servicos = servicos,
+                
+
+        };
+
+            return base.View(modelo);
+        }
+
+   
+
+
+
+    }
+}
---
+++ Models/HomeGestaoViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto_Lab_Web_Grupo3.Models
+{
+    public class HomeGestaoViewModel
+    {
+
+        public List<Utilizadores> Utilizadores { get; set; }
+
+
+        public List<Promocoes> Promocoes { get; set; }
+
+        public List<Servicos> Servicos { get; set; }
+
+        public List<ServicosPacotes> ServicosPacotes { get; set; }
+
+        public List<PromocoesPacotes> PromocoesPacotes { get; set; }
+
+        public List<Pacotes> Pacotes { get; set; }
+
+        public List<Contratos> Contratos { get; set; }
+
+    }
+}
