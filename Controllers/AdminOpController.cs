c1b2579c4e5f5c471ac9f1e52838ebf10942ca00
LendeaViva <73750123+LendeaViva@users.noreply.github.com>
2021-03-04 12:02:45 +0000

    Vista geral de pacotes, promos e serviços a 70%

--- Controllers/AdminOpController.cs
+++ Controllers/AdminOpController.cs
@@ -11,25 +11,68 @@ namespace Projeto_Lab_Web_Grupo3.Controllers
 {
     public class AdminOpController : Controller
     {
-     
-            private readonly Projeto_Lab_WebContext bd;
 
-            public AdminOpController(Projeto_Lab_WebContext context)
-            {
-                bd = context;
-            }
+        private readonly Projeto_Lab_WebContext bd;
 
-         
-            public async Task<IActionResult> Index()
-            {
-            
-
-                };
-
-            return base.View(modelo);
+        public AdminOpController(Projeto_Lab_WebContext context)
+        {
+            bd = context;
         }
 
-   
+
+        public async Task<IActionResult> Index()
+        {
+            List<InfoPacoteViewModel> ListaPacotes = new List<InfoPacoteViewModel>();
+
+            foreach (var pacote in bd.Pacotes)
+            {
+                InfoPacoteViewModel infoPacote = new InfoPacoteViewModel();
+                infoPacote.Pacote = pacote;
+
+                infoPacote.Promocao = await bd.PromocoesPacotes
+                    .Include(p => p.Promocoes)
+                    .Where(p => p.PacoteId == pacote.PacoteId)
+                    .Select(p => p.Promocoes)
+                    .Where(p => p.DataInicio < DateTime.Now && p.DataFim > DateTime.Now)
+                    .FirstOrDefaultAsync();
+
+                infoPacote.Servicos = await bd.ServicosPacotes
+                    .Include(p => p.Servico)
+                    .Where(p => p.PacoteId == pacote.PacoteId)
+                    .Select(p => p.Servico)
+                    .ToListAsync();
+
+                 
+                if (infoPacote.Promocao != null)
+                {
+                    decimal PrecoPacote = infoPacote.Pacote.Preco;
+                    decimal Desc = infoPacote.Promocao.PromocaoDesc;
+                    //esta conta não funciona :(
+                    decimal PrecoFinal = PrecoPacote - Desc; 
+                    ViewData["PrecoFinal"] = PrecoFinal;
+                }
+                                
+                else
+                {
+
+                    ViewData["PrecoFinal"] = "";               
+
+                }
+         
+                ListaPacotes.Add(infoPacote);
+            }
+
+            HomeGestaoViewModel modelo = new HomeGestaoViewModel
+            {
+                Pacotes = ListaPacotes,
+                            
+                
+            };
+
+            return View(modelo);
+        }
+
+
 
 
 
