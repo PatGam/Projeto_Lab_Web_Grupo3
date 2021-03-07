52d175f37ffb6b3807d7d5967cc23039aa21837a
LendeaViva <73750123+LendeaViva@users.noreply.github.com>
2021-03-07 10:36:26 +0000

    Detalhes e Editar dos Clientes completo

--- Models/Contratos.cs
+++ Models/Contratos.cs
@@ -33,9 +33,16 @@ namespace Projeto_Lab_Web_Grupo3.Models
 
         public Promocoes Promocoes { get; set; }
 
+
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        [DataType(DataType.Date)]
         [Column("Data_inicio", TypeName = "date")]
         [Display(Name = "Data de Início")]
         public DateTime DataInicio { get; set; }
+
+
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        [DataType(DataType.Date)]
         [Column("Data_Fim", TypeName = "date")]
         [Display(Name = "Data de Fim")]
         public DateTime DataFim { get; set; }
