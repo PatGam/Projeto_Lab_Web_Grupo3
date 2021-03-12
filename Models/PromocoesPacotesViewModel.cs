cb43b7f0376ae372b0136c5645e7e61cb97801d0
Nuno Fonseca <nuno_rgf@hotmail.com>
2021-03-12 11:37:16 +0000

    correção do editar dos pacotes e mais algumas alterações

--- Models/PromocoesPacotesViewModel.cs
+++ Models/PromocoesPacotesViewModel.cs
@@ -20,18 +20,28 @@ namespace Projeto_Lab_Web_Grupo3.Models
 
 
         public int PromocoesId { get; set; }
-        [Required]
+        [Required ( ErrorMessage ="Tem de Preencher o nome")]
         [StringLength(100)]
         public string Nome { get; set; }
+
         [StringLength(1000)]
         [Display(Name = "Descrição")]
         public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "Tem de Preencher a data de início")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        [DataType(DataType.Date)]
         [Column("Data_inicio", TypeName = "date")]
         [Display(Name = "Data Início")]
         public DateTime DataInicio { get; set; }
+
+        [Required(ErrorMessage = "Tem de Preencher a data de fim")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        [DataType(DataType.Date)]
         [Column("Data_fim", TypeName = "date")]
         [Display(Name = "Data de Fim")]
         public DateTime DataFim { get; set; }
+
         [Column("Promocao_desc", TypeName = "decimal(18, 2)")]
         [RegularExpression(@"^([0-9]*[1-9][0-9]*(\,[0-9]+)?|[0]+\,[0-9]*[1-9][0-9]*)$", ErrorMessage = "O valor tem ser superior a 0")]
         [Display(Name = "Desconto da Promoção")]
