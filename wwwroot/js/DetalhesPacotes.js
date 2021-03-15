980c14d0fec6f031ccb22f2fb6176f0fd38b62f8
MajestosoPum <dark.mew69@gmail.com>
2021-03-15 06:42:08 +0000

    Adicionados Pacotes Corrigidas vistas Update Página Televisão

---
+++ wwwroot/js/DetalhesPacotes.js
@@ -0,0 +1,9 @@
+﻿$(function () {
+    $(".details").click(function () {
+        var id = $(this).attr("data-id");
+        alert(id);
+        $("#modal").load("Details?id=" + id, function () {
+            $("#modal").modal();
+        })
+    });
+})
