a0ac504637f942b9ad4b4a5b51f1adc9edd4ca5d
PatGam <patricia.m.gameiro@upskill.pt>
2021-03-22 15:33:40 +0000

    Top10 Clientes mais antigos - por testar

---
+++ Controllers/Top10Controller.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Projeto_Lab_Web_Grupo3.Data;
+using Projeto_Lab_Web_Grupo3.Models;
+
+
+namespace Projeto_Lab_Web_Grupo3.Controllers
+{
+    public class Top10Controller : Controller
+    {
+        private readonly Projeto_Lab_WebContext bd;
+
+        public Top10Controller(Projeto_Lab_WebContext context)
+        {
+            bd = context;
+        }
+
+        public async Task<IActionResult> Index(string nifpesquisar, bool inactivo, int pagina = 1)
+        {
+          
+                List<Utilizadores> clientesAntigos = await bd.Utilizadores
+                    .OrderByDescending(p => p.DataAtivacao)
+                    .Take(10)
+                    .ToListAsync();
+
+            Top10ViewModel top10clientesAntigos = new Top10ViewModel
+            {
+                   Utilizadores  = clientesAntigos,
+                             };
+                return View(top10clientesAntigos);
+            }
+        
+    }
+}
