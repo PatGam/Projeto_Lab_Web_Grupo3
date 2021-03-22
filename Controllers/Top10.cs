980b1af1f03bb1625dd00b96f4e7d3d75d205475
PatGam <patricia.m.gameiro@upskill.pt>
2021-03-22 15:27:52 +0000

    Criado controlador e viewmodel Top10;

---
+++ Controllers/Top10.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto_Lab_Web_Grupo3.Controllers
+{
+    public class Top10 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Top10ViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto_Lab_Web_Grupo3.Models
+{
+    public class Top10ViewModel
+
+    {
+        public List<Contratos> Contratos { get; set; }
+        public List<Utilizadores> Utilizadores { get; set; }
+        public int UtilizadorId { get; set; }        
+    }
+}
