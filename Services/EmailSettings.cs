579d5721b4282490be0b4682f1040f3fadb36f08
MajestosoPum <dark.mew69@gmail.com>
2021-03-28 23:46:06 +0100

    Enviar E-mail funcional

---
+++ Services/EmailSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto_Lab_Web_Grupo3.Services
+{
+    public class EmailSettings
+    {
+        public String PrimaryDomain { get; set; }
+        public int PrimaryPort { get; set; }
+        public String UsernameEmail { get; set; }
+        public String UsernamePassword { get; set; }
+        public String FromEmail { get; set; }
+        public String ToEmail { get; set; }
+        public String CcEmail { get; set; }
+    }
+}
---
+++ Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto_Lab_Web_Grupo3.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
