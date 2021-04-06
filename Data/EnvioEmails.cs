97ca49b31fb5d047124d943082a0a91872db7546
PatGam <patricia.m.gameiro@upskill.pt>
2021-04-06 17:55:37 +0100

    Progresso no envio dos E-mails

---
+++ Data/EnvioEmails.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Identity.UI.V3.Pages.Account.Manage.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto_Lab_Web_Grupo3.Data
+{
+    public class EnvioEmails
+    {
+        private readonly Projeto_Lab_WebContext bd;
+
+        public EnvioEmails(Projeto_Lab_WebContext context)
+        {
+            bd = context;
+        }
+
+        private readonly IEmailSender _emailSender;
+
+        public EnvioEmails(Projeto_Lab_WebContext context, IEmailSender emailSender)
+        {
+            bd = context;
+            _emailSender = emailSender;
+        }
+
+
+        public async Task TesteEnvioEmail(string email, string assunto, string mensagem)
+        {
+            foreach(var item in bd.Contratos)
+            {
+                try
+                {
+                  
+                    //email destino, assunto do email, mensagem a enviar
+                    await _emailSender.SendEmailAsync(email, assunto, mensagem);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+           
+        }
+    }
+}
